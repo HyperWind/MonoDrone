3865a60fa84c17c28b6ee2ae085565a7c2938fea
Karolis Lasys (aka. HyperWind) <karolioofficial@outlook.com>
2015-03-30 14:40:01 +0300

    fixed a bug in the AT packets that made the Seq_nr not itarate correcty and remade the main module.

--- Source/Drone_AT_Packet.cs
+++ Source/Drone_AT_Packet.cs
@@ -8,14 +8,10 @@ namespace MonoDrone
 	public class AT { // AT commands
 
 		private static int Seq_nr = 0;
-
 		private int[] AT_Args;
-
 		private string AT_Type;
 
 		public AT (string type, params int[] args) {
-
-			Seq_nr++;
 
 			AT_Type = type;
 			AT_Args = args;
@@ -24,6 +20,7 @@ namespace MonoDrone
 
 		public string Serialize () {
 
+			Seq_nr++;
 			return  "AT*" + AT_Type + "=" + Seq_nr + "," + string.Join (",", AT_Args) + "\r";
 
 		}
--- Source/Drone_Main.cs
+++ Source/Drone_Main.cs
@@ -6,28 +6,78 @@ using MonoDrone;
 
 namespace MonoDrone {
 
+	/*
+	 * <summary>
+	 * Keeps the drone status and the following command.
+	 * </summary>
+	*/
+	public class DroneStatus {
+
+		public bool Flying = false; // Flying or not.
+		public int Flags = 1; // Flag of the comming command. 
+		public double TiltLR = 0; // Forwards/backwards tilt.
+		public double TiltFB = 0; // Left/Right tilt.
+		public double Vert = 0; // Vertical speed, aka. the amount you move up or down.
+		public double Ang = 0;	// Angular speed, aka. the amount you turn left or right.
+		public int LastAlt = 0; // Last altitude.
+		public int Battery = 100; // Battery left.
+
+		/*
+		 * <summary>
+		 * Resets the important bits, so you don't have to.
+		 * </summary>
+		*/
+		public void Reset () {
+
+			Flags = 1;
+			TiltLR = 0;
+			TiltFB = 0;
+			Vert = 0;
+			Ang = 0;
+
+		}
+			
+	}
+
+	/*
+	 * <summary> 
+	 * The exception thrown if a value passed to any of the movement commands is out of bounds (1 >= val >= -1).
+	 * </summary>
+	*/ 
 	class CommandOutOfBounds : Exception {
 		public CommandOutOfBounds () {}
 	}
 
 	public class Drone {
 
-		private static int REPLY_PORT = 5554; // UDC
-		private static int VIDEO_PORT = 5555; // UDC
-		private static int COMMAND_PORT = 5556; // UDC
-		private static IPAddress IP; // ip address
-		private	Sender sends;
-		private Getter gets;
+		private static int REPLY_PORT = 5554; // UDC port for drone navdata.
+		private static int VIDEO_PORT = 5555; // UDC port for drone video. (not yet implamented!)
+		private static int COMMAND_PORT = 5556; // UDC port for sending commands to the drone.
+		private static IPAddress IP; // Ip address.
+		private	Sender Sends; // Sender object.
+		private Getter Gets; // Getter object.
+		private DroneStatus Status; // Status class, keeps everything in check.
 
+		/*
+		 * <summary> 
+		 * Initializes the drone.
+		 * </summary>
+		*/
 		public Drone (string ip="192.168.1.1") {
 			
 			IP = IPAddress.Parse (ip);
 
-			sends = new Sender (IP, COMMAND_PORT);
-			gets = new Getter (REPLY_PORT);
+			Status = new DroneStatus ();
+			Sends = new Sender (IP, COMMAND_PORT);
+			Gets = new Getter (REPLY_PORT);
 		
 		}
 
+		/*
+		 * <summary> 
+		 * Transforms the value given to an integer word, aka. does magic and checks if the value is in bounds.
+		 * </summary>
+		*/
 		private unsafe int DoubleRefToInt (double conv) {
 
 			int converted = 0;
@@ -56,7 +106,12 @@ namespace MonoDrone {
 		
 		}
 
-		public void Move (int flags, double _LeftRightTilt = 0, double _FrontBackTilt = 0, double _VerticalSpeed = 0, double _AngularSpeed = 0) {
+		/*
+		 * <summary> 
+		 * Serializes the command and sends it to the mothership.
+		 * </summary>
+		*/
+		private void Move (int flags, double _LeftRightTilt = 0, double _FrontBackTilt = 0, double _VerticalSpeed = 0, double _AngularSpeed = 0) {
 
 			int LeftRightTilt = DoubleRefToInt (_LeftRightTilt);
 			int FrontBackTilt = DoubleRefToInt (_FrontBackTilt);
@@ -66,69 +121,235 @@ namespace MonoDrone {
 			AT at = new AT ("PCMD", flags, LeftRightTilt, FrontBackTilt, VerticalSpeed, AngularSpeed);
 			AT buffer = new AT ("PCMD", 1, 0, 0, 0, 0);
 	
-			sends.Send (at.Serialize ());
-			sends.Send (buffer.Serialize ());	
+			Sends.Send (at.Serialize ());
+			Sends.Send (buffer.Serialize ());	
 		}
 
-/*		public void Forwards (double amount) {
-			Move (1);
+		/*
+		 * <summary>
+		 * Makes the drone move forwards, only allows commands from 1 to 0.
+		 * </summary>
+		*/
+		public void Forwards (double amount) {
+
+			try {
+
+				if (amount >= 0 && amount <= 1) {
+				
+					Status.TiltFB = amount;
+					Status.Flags = 1;
+
+				} else {
+
+					throw new CommandOutOfBounds();
+
+				}
+
+			} catch (Exception err) {
+
+				Console.Error.WriteLine ("{0} Argument cannot be over 1 or negative.", err);
+
+			}
+
 		}
 
+		/*
+		 * <summary>
+		 * Makes the drone move backwards, also accepts only positive numbers (and 0) up to 1.
+		 * </summary>
+		*/
 		public void Backwards (double amount) {
-			Move (1);
+
+			try {
+
+				if (amount >= 0 && amount <= 1) {
+
+					Status.TiltFB = -amount;
+					Status.Flags = 1;
+
+				} else {
+
+					throw new CommandOutOfBounds();
+
+				}
+
+			} catch (Exception err) {
+
+				Console.Error.WriteLine("{0} Argument cannot be over 1 or negative.", err);
+
+			}
+
 		}
 	
-		public void StrafeLeft (double amount) {
-			Move (1);
+		/*
+		 * <summary>
+		 * Makes the drone strafe left or right, that depends on the sign of the number.
+		 * (positive - left, negative - right)
+		 * </summary>
+		*/
+		public void Strafe (double amount) {
+			Status.TiltLR = amount;
+			Status.Flags = 1;
 		}
 
-		public void StrafeRight (double amount) {
-			Move (1);
+		/*
+		 * <summary> 
+		 * Makes the drone turn left or right, also dictated by the sign of the number.
+		 * (positive - left, negative - right)
+		 * </summary>
+		 * 
+		 * <suggestion>
+		 * Make it accept degrees instead?
+		 * </suggestion>
+		*/
+		public void Turn (double amount) {
+			Status.Ang = amount;
+			Status.Flags = 1;
 		}
 
-		public void TurnLeft (double amount) {
-			Move (1);
-		}
-
-		public void TurnRight (double amount) {
-			Move (1);
-		}
-
+		/*
+		 * <summary> 
+		 * Makes the thing go up, 0 <= amount <= 1.
+		 * </summary>
+		*/
 		public void Up (double amount) {
-			Move (1);
+
+			try {
+
+				if (amount >= 0 && amount <= 1) {
+
+					Status.Vert = amount;
+					Status.Flags = 1;
+
+				} else {
+
+					throw new CommandOutOfBounds();
+
+				}
+
+			} catch (Exception err) {
+
+				Console.Error.WriteLine ("{0} Argument cannot be over 1 or negative.", err);
+
+			}
+
 		}
 	
+		/*
+		 * <summary> 
+		 * Makes it go down (wow), again, the 'amount' can't be over 1 or negative.
+		 * </summary>
+		*/
 		public void Down (double amount) {
-			Move (1);
+
+			try {
+
+				if (amount >= 0 && amount <= 1) {
+
+					Status.Vert = -amount;
+					Status.Flags = 1;
+
+				} else {
+
+					throw new CommandOutOfBounds();
+
+				}
+
+			} catch (Exception err) {
+
+				Console.Error.WriteLine ("{0} Argument cannot be over 1 or negative.", err);
+
+			}
 		}
-*/		
+
+		/*
+		 * <summary>
+		 * Makes the thing hover.
+		 * </summary>
+		*/
 		public void Hover () {
-
-			Move (0);
-		
+			Status.Reset ();
+			Status.Flags = 0;
 		}
 
+		/*
+		 * <summary>
+		 * After all of the wanted commands have been issued - sends them to the drone and resets the state.
+		 * 
+		 * If the drone isn't flying - throws a warning instead and resets the state.
+		 * </summary>
+		*/
+		public void Send () {
+
+			if (Status.Flying == true) {
+
+				Move (Status.Flags, Status.TiltLR, Status.TiltFB, Status.Vert, Status.Ang); 
+
+			} else {
+
+				Console.Error.WriteLine ("MonoDrone Warning - Drone must be flying first!");
+
+			}
+				
+			Status.Reset ();
+
+		}
+
+		/*
+		 * <summary>
+		 * Lands the drone.
+		 * </summary>
+		*/
 		public void Land () {
 
+			Status.Flying = false;
 			AT at = new AT ("REF", 290717696);
-			sends.Send (at.Serialize ());
+			Sends.Send (at.Serialize ());
 		
 		}
 
+		/*
+		 * <summary>
+		 * As the name suggests, makes the drone take off.
+		 * </summary>
+		*/
 		public void TakeOff () {
 
+			Status.Flying = true;
 			AT at = new AT ("REF", 290718208);
-			sends.Send (at.Serialize ());		
+			Sends.Send (at.Serialize ());		
 
 		}
 
-		public void Wait (int seconds, Action action) {
+		/*
+		 * <summary> 
+		 * Returns the state of the drone as a formatted string.
+		 * </summary>
+		*/
+		public string State (bool Altitude = true, bool Battery = true, bool Flying = true, bool Command = true) {
 
-			Thread.Sleep (seconds * 1000);
+			string state = "";
 
-			action ();
+			if (Altitude == true) 
+				state = state + String.Format ("Altitude: {0}\r", Status.LastAlt);
+			if (Battery == true)
+				state = state + String.Format ("Battery left: {0}%\r", Status.Battery);
+			if (Flying == true)
+				state = state + String.Format ("Flying: {0}\r", Status.Flying);
+			if (Command == true) {
+				state = state + String.Format (
+					"Next Command: {0}, {1}, {2}, {3}, {4}\r",
+					Status.Flags,
+					Status.TiltFB,
+					Status.TiltLR,
+					Status.Vert,
+					Status.Ang
+				);
+			}
 
-		} 
+			return state;
+
+		}
 
 	}
 
